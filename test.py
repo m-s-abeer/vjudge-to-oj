a0509026288b061c46f278823e81b118e030b573
Mahmud Sajjad Abeer <mahmudsajjad.abeer@gmail.com>
2020-05-18 12:35:43 +0600

    test

--- test.py
+++ test.py
@@ -4,17 +4,3 @@ UVaPassword = "pass"
 
 uvaUser = judges.UVA(UVaUserName, UVaPassword)
 uvaUser.submitAll()
-
-
-# from modules import apiHandler
-# from modules import judges
-
-# # apicaller = apiHandler.ApiCaller()
-
-# # apicaller.getUvaProblemNumberFromProblemId(36)
-
-
-# UVaUserName = "abeer6764"
-# UVaPassword = "testpass"
-
-# uvaUser = judges.UVA(UVaUserName, UVaPassword)
