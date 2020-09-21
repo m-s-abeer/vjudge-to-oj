c5ab9a23dca425eb479031511d51c6fb87cd2e28
Mahmud Sajjad Abeer <mahmudsajjad.abeer@gmail.com>
2020-09-21 15:26:05 +0600

    Bugfix: missing UvaProblem class included

--- modules/submissions.py
+++ modules/submissions.py
@@ -47,6 +47,77 @@ class Solution:
     def __str__(self):
         return self.problemNumber + " - " + self.problemName
 
+class UvaProblem(Problem):
+    problemId = str()
+    problemName = str()
+    savingPath = path + os.sep + "Submitted" + os.sep + "UVA"
+
+    def __init__(self, problemDir, problemNumber, judgeSlug = "UVA"):
+        super().__init__(problemDir, problemNumber, judgeSlug)
+        problemData = apicaller.getUvaProblemDataUsingProblemNumberOffline(problemNumber)
+        self.problemId = str(problemData[0])
+        self.problemName = str(problemData[1])
+        solCnt = 0
+        for subName in os.listdir(problemDir):
+            self.solutions.append(list([Solution(problemDir + os.sep + subName, problemNumber, self.problemName), solCnt]))
+            solCnt = solCnt + 1
+
+    """
+    Important when saving solution.
+    Fobidden printable ascii characters while saving:-
+
+    Linux/Unix:
+    / (forward slash)
+    
+    Windows:
+    < (less than)
+    > (greater than)
+    : (colon - sometimes works, but is actually NTFS Alternate Data Streams)
+    " (double quote)
+    / (forward slash)
+    \ (backslash)
+    | (vertical bar or pipe)
+    ? (question mark)
+    * (asterisk)
+
+    """
+
+    def fileNameCleaner(self, fileName):
+        forbidden = {'/', '\\', '<', '>', ':', '"', '|', '?', '*'}
+        newName = ""
+        for c in fileName:
+            if(c not in forbidden):
+                newName = newName + str(c)
+        return newName
+
+    def saveSolution(self, solutionId, submissionId):
+        fileName = self.problemNumber + " - " + self.problemName
+        fileName = fileName + "(" + str(solutionId) + ", " + str(submissionId) + ")." + self.solutions[solutionId][0].solutionExt
+        fileName = self.fileNameCleaner(fileName)
+        savePath = self.savingPath + os.sep + fileName
+        print("Saving on : ", savePath)
+        with self.safe_open_w(savePath) as f:
+            f.write(self.solutions[solutionId][0].solutionCode)
+
+    def mkdir_p(self, path):
+        try:
+            os.makedirs(path)
+            if(os.path.exists(path)):
+                print("path created on ", path)
+            else:
+                print("path not created")
+            
+        except OSError as exc: # Python >2.5
+            if exc.errno == errno.EEXIST and os.path.isdir(path):
+                pass
+            else: raise
+
+    def safe_open_w(self, path):
+        ''' Open "path" for writing, creating any parent directories as needed.
+        '''
+        self.mkdir_p(os.path.dirname(path))
+        return open(path, 'w')
+
 class CodeForcesProblem(Problem):
     problemId = str()
     problemName = str()
